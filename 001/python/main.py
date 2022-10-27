5cfe789926546ae5e80f270f44a33edab5e9d514
Nick Iturraran <nicholas.iturraran@gmail.com>
2022-10-27 09:05:16 -0700

    updated 001

--- 001/python/main.py
+++ 001/python/main.py
@@ -6,10 +6,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 
-divisible_numbers = []
-for i in range(1, 1000):
-    if (i % 3 == 0) or (i % 5 == 0):
-        divisible_numbers.append(i)
+divisible_numbers = [x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0]
 
 ans = sum(divisible_numbers)
-print(ans)
+print(f"ans == {ans}")

f112e39b6e78006d7658026c6784fe79bc8cb83c
Nick Iturraran <nicholas.iturraran@gmail.com>
2022-10-29 14:22:48 -0700

    solution for 053

---
+++ 053/main.py
@@ -0,0 +1,40 @@
+"""
+There are exactly ten ways of selecting three from five, 12345:
+
+123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+In combinatorics, we use the notation, nCr.
+
+In general,
+
+nCr = n! / r!(n-r)!, where r <= n, n! = n * (n-1) * ... * 3 * 2 * 1, and 0! = 1.
+
+It is not until n = 23, that a value exceeds one-million: 23 choose 10 = 1144066
+.
+
+How many, not necessarily distinct, values of nCr for 1 <= n <= 100, are greater than one-million?
+
+---
+Method:
+
+
+
+"""
+
+from math import factorial
+
+
+def comb(n, r):
+    return int(factorial(n) / (factorial(r) * factorial(n - r)))
+
+
+limit = 1000000
+count = 0
+for n in range(1, 100 + 1):
+    for r in range(n):
+        if comb(n, r) > limit:
+            count += 1
+
+ans = count
+print(f"ans == {ans}")
+
